a7127cedbb3f9baed8b252da6e46e05cacdbe3b8
Ashmit111 <ashmitsingh862@gmail.com>
2025-01-07 22:21:04 +0530

    ..

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Textarea } from "@/components/ui/textarea";
+import { useForm } from 'react-hook-form';
+import { messageSchema } from '@/schemas/messageSchema';
+
 
 const usermessage = () => {
+  const onSubmit = async (data:any) => {
+
+  }
+  const form = useForm
   return (
-    <div>
-      usermessage
+    <div className='bg-white w-full my-8 mx-4 p-6'>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='p-3'>
+          <h1 className='text-black text-4xl font-bold'>Public Profile Link</h1>
+        </div>
+         <Form>
+            <form onSubmit={onSubmit} className="space-y-6">
+
+            </form>
+         </Form>
+      </div>
     </div>
   )
 }
