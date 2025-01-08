7ff65446c82087f41f2df0310c9a8924a0541ea7
Ashmit111 <ashmitsingh862@gmail.com>
2025-01-08 23:00:53 +0530

    Add message status fetching and improve form layout in user message component

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import { useParams } from 'next/navigation';
 import * as z from 'zod';
 import { Button } from '@/components/ui/button';
@@ -17,13 +17,17 @@ import { useForm } from 'react-hook-form';
 import { messageSchema } from '@/schemas/messageSchema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Loader2 } from 'lucide-react';
-
+import axios from 'axios';
+import { ApiResponse } from '@/types/ApiResponse';
+import { AxiosError } from 'axios';
+import { useToast } from '@/components/ui/use-toast';
 
 const usermessage = () => {
   const params = useParams<{ username: string }>();
+  const [messagestatus, setMessagestatus] = useState<boolean>(false);
   const username = params.username;
   const [isLoading, setIsLoading] = useState(false);
-
+  const { toast } = useToast();
   const onSubmit = async (data:any) => {
 
   }
@@ -31,47 +35,66 @@ const usermessage = () => {
     resolver: zodResolver(messageSchema),
   })
 
+  const fetchAcceptMessages = async () =>{
+    try {
+      const response = await axios.get<ApiResponse>('/api/accept-messages');
+      const status = response.data.isAcceptingMessage;
+      setMessagestatus(status ?? false);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+        toast({
+          title: 'Error',
+          description:
+          axiosError.response?.data.message ??
+          'Failed to fetch message status',
+          variant: 'destructive',
+        });
+    }
+  }
+  useEffect(() => {
+    fetchAcceptMessages();
+  }, [])
+  
+
   const messageContent = form.watch('content');
   return (
-    <div className='bg-white w-full my-8 mx-4 p-6'>
-      <div className='flex flex-col justify-center items-center'>
-        <div className='p-3'>
-          <h1 className='text-black text-4xl font-bold'>Public Profile Link</h1>
-        </div>
-         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-              control={form.control}
-              name='content'
-              render={({field}) => (
-                <FormItem>
-                  <FormLabel>Send Anonymous Message to @{username}</FormLabel>
-                  <FormControl>
-                    <Textarea
-                    placeholder='Write your anonymous message here'
-                    className="resize-none w-max"
+     <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
+      <h1 className="text-4xl font-bold mb-6 text-center">
+        Public Profile Link
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Send Anonymous Message to @{username}</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your anonymous message here"
+                    className="resize-none"
                     {...field}
-                    />
-                  </FormControl>
-                  <FormMessage /> 
-                </FormItem>
-              )}
-              />
-              <div>
-                {isLoading ?(
-                  <Button disabled>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
-                    Please Wait
-                  </Button>
-                ):(
-                  <Button type="submit" disabled={isLoading || !messageContent}>
-                    Send Message
-                  </Button>
-                )}
-              </div>
-            </form>
-         </Form>
-      </div>
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="flex justify-center">
+            {isLoading ? (
+              <Button disabled>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Please wait
+              </Button>
+            ) : (
+              <Button type="submit" disabled={isLoading || !messageContent}>
+                Send It
+              </Button>
+            )}
+          </div>
+        </form>
+      </Form>
     </div>
   )
 }
