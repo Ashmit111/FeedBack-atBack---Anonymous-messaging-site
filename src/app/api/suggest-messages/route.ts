2aa1002457bbf4e655912bd4ab3d1c9651142ce2
Ashmit111 <ashmitsingh862@gmail.com>
2025-01-11 20:20:24 +0530

    Integrated Gemini

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,54 +1,44 @@
-import { OpenAI } from "openai";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-//USE CASE
+const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 
-//1. User will click on suggest-message
-//2. we will go to open ai with some prompts
-//3. we will show the response on frontend
+if (!apiKey) {
+  throw new Error("GEMINI_API_KEY is not defined in environment variables.");
+}
 
-const openai = new OpenAI({     
-  apiKey: process.env.OPENAI_API_KEY,
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-pro",
 });
 
-export const runtime = "edge";
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 40,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
 
-export async function POST(req: Request) {
+const PROMPT = `Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.All questions should be unique`;
+
+export async function POST(req: NextRequest) {
   try {
-    const prompt =
-      "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What’s a hobby you’ve recently started?||If you could have dinner with any historical figure, who would it be?||What’s a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
-
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
-      max_tokens: 400,
-      stream: true,
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [],
     });
 
-    const stream = new ReadableStream({
-      async start(controller) {
-        for await (const chunk of response) {
-          const { choices } = chunk;
-          if (choices && choices.length > 0) {
-            const text = choices[0].delta?.content || "";
-            controller.enqueue(text);
-          }
-        }
-        controller.close();
-      },
-    });
+    const result = await chatSession.sendMessage(PROMPT);
+    console.log(result.response.text());
 
-    return new Response(stream, {
-      headers: { "Content-Type": "text/plain" },
-    });
+    return NextResponse.json({ response: result.response.text() });
   } catch (error) {
-    if (error instanceof OpenAI.APIError) {
-      // OpenAI API error handling
-      const { name, status, headers, message } = error;
-      return NextResponse.json({ name, status, headers, message }, { status });
-    } else {
-      console.error("An unexpected error occurred:", error);
-      throw error;
-    }
+    console.error("Error generating suggestions:", error);
+    return NextResponse.json(
+      { error: "Failed to generate suggestions" },
+      { status: 500 }
+    );
   }
 }
